4970a45dbad16c588ac9b9ffdf84156a1abcec1e
gin <gin2798@gmail.com>
2020-07-02 02:28:04 +0900

    26進法　むずかった。

--- abc171/c.py
+++ abc171/c.py
@@ -1,28 +1,14 @@
-# 最下位から求めていく。
-# nを受け取る
-# i は 0　スタート
-# n % 26 = x
-# n から　x + 26 ** iを引く
-# n　が 0 になるまで繰り返す
+N = int(input())
 
-def addChar(x):
-	global ans
-	addC = ''
-	if x == 0:
-		addC = 'z'
-	else:
-		addC = chr(96+x)
-	ans = ans + addC
+ans = ''
+for i in range(1,99):
+    if N <= 26**i:
+        N-=1
+        for j in range(i):
+            ans = chr(ord('a')+(N%26)) + ans
+            N = N // 26
+        break;
+    else:
+        N -= 26**i
 
-n = int(input())
-i = 0
-d = []
-while(n > 26):
-	d.append(n%26)
-	n = n//26
-print(d)
-d.append(n)
-
-print(d)
-
-	
+print(ans)
